47339d9bc7568ef72ae9de14cdc51f4b25a8fae4
Yuliia <121313266+Yuliia-K27@users.noreply.github.com>
2023-03-01 15:05:14 +0200

    Update L12-1.py
    new commit

--- L12-1.py
+++ L12-1.py
@@ -1,41 +1,37 @@
-import os
-import threading
+import math
 import time
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 
-from concurrent.futures import ThreadPoolExecutor
-from concurrent.futures import ProcessPoolExecutor
+def factorial(n):
+    time.sleep(1)  # імітуємо важку обчислювальну операцію
+    return math.factorial(n)
 
-def fact(n):
-    if n<0:
-        return print("Оnly positive number")
-    elif n==0:
-        return 1
-    else:
-        return n*fact(n-1)
+def calculate_factorial_threadpool():
+    with ThreadPoolExecutor() as executor:
+        futures = [executor.submit(factorial, i) for i in range(1, 11)]
+        results = [future.result() for future in futures]
+        return results
 
-with ThreadPoolExecutor() as executor:
-    start1 = time.time()
-    f1=executor.submit(fact, 900)
-    f2=executor.submit(fact, 800)
-    f3=executor.submit(fact, 600)
-    f1.result()
-    f2.result()
-    f3.result()
-    finish1 = time.time()
-    t1 = finish1 - start1
+def calculate_factorial_processpool():
+    with ProcessPoolExecutor() as executor:
+        futures = [executor.submit(factorial, i) for i in range(1, 11)]
+        results = [future.result() for future in futures]
+        return results
 
 if __name__ == '__main__':
-    start2 = time.time()
-    with ProcessPoolExecutor() as executor:
-        res=[]
-        res=executor.map(fact, [900, 800, 600])
-    finish2=time.time()
-    t2=finish2-start2
+    start_time = time.time()
+    calculate_factorial_threadpool()
+    end_time = time.time()
+    time_threadpool = end_time - start_time
+    print(f'ThreadPoolExecutor time: {time_threadpool}')
 
-    if t1>t2:
-        print("ThreadPoolExecutor() is better of ProcessPoolExecutor()")
-    elif t1<t2:
-        print("ProcessPoolExecutor() is better of ThreadPoolExecutor()")
+    start_time = time.time()
+    calculate_factorial_processpool()
+    end_time = time.time()
+    time_processpool = end_time - start_time
+    print(f'ProcessPoolExecutor time: {time_processpool}')
+
+    if time_threadpool < time_processpool:
+        print('ThreadPoolExecutor is faster')
     else:
-        print("ProcessPoolExecutor() equally ThreadPoolExecutor()")
-
+        print('ProcessPoolExecutor is faster')
