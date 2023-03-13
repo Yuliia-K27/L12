adbae45d5768e1309487b2d67342e061f5077005
Yuliia <121313266+Yuliia-K27@users.noreply.github.com>
2023-03-14 00:37:16 +0200

    commit - Виправлення зауважень
    Виправлення зауважень:
    Question 1:
    Э декілька побажань по коду. У функції factorial не перевіряється, чи введене число є цілим і більшим або рівним 0, що може призвести до помилки в обчисленні факторіалу.
    
    Для порівняння швидкості обчислень необхідно запустити функції calculate_factorial_threadpool та calculate_factorial_processpool кілька разів і взяти середнє значення часу виконання.

--- L12-1.py
+++ L12-1.py
@@ -3,6 +3,8 @@ import time
 from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 
 def factorial(n):
+    if not isinstance(n, int) or n < 0:
+        raise ValueError("n must be a non-negative integer")
     time.sleep(1)  # імітуємо важку обчислювальну операцію
     return math.factorial(n)
 
@@ -19,19 +21,26 @@ def calculate_factorial_processpool():
         return results
 
 if __name__ == '__main__':
-    start_time = time.time()
-    calculate_factorial_threadpool()
-    end_time = time.time()
-    time_threadpool = end_time - start_time
-    print(f'ThreadPoolExecutor time: {time_threadpool}')
+    results = []
+    for i in range(5):
+        start_time = time.time()
+        calculate_factorial_threadpool()
+        end_time = time.time()
+        time_threadpool = end_time - start_time
+        print(f'ThreadPoolExecutor time: {time_threadpool}')
+        results.append(time_threadpool)
 
-    start_time = time.time()
-    calculate_factorial_processpool()
-    end_time = time.time()
-    time_processpool = end_time - start_time
-    print(f'ProcessPoolExecutor time: {time_processpool}')
+        start_time = time.time()
+        calculate_factorial_processpool()
+        end_time = time.time()
+        time_processpool = end_time - start_time
+        print(f'ProcessPoolExecutor time: {time_processpool}')
+        results.append(time_threadpool)
+        
+    avg_time_threadpool = sum(results[::2]) / 5
+    avg_time_processpool = sum(results[1::2]) / 5
 
-    if time_threadpool < time_processpool:
+    if avg_time_threadpool < avg_time_processpool:
         print('ThreadPoolExecutor is faster')
     else:
         print('ProcessPoolExecutor is faster')
